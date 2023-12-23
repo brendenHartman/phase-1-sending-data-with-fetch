679c370dc1af54e7bc9ec50f3e4945c3684675a8
Brenden Hartman <brendenhart22@gmail.com>
2023-12-23 12:47:34 -0600

    yay

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users',
+        {method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+         },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+    .then(function(resp){return resp.json();})
+    .then(function(json){ 
+        const id = json.id;
+        const newNum = document.createElement('p');
+        newNum.textContent = id;
+        document.body.appendChild(newNum);
+    })
+    .catch(function(error){
+        const newError = document.createElement('p');
+        newError.textContent = error.message;
+        document.body.appendChild(newError);
+    })
+}
